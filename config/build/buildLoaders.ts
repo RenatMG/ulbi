a3ebbf36e1c82443541d81bc1153e7982b7367fc
Renat <r.detta@yandex.ru>
2023-01-29 20:43:44 +0400

    Webpack. Введение в арх-ру. Code splitting. Css Modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,11 +5,8 @@ export function buildLoaders(): webpack.RuleSetRule[] {
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            // Creates `style` nodes from JS strings
             "style-loader",
-            // Translates CSS into CommonJS
             "css-loader",
-            // Compiles Sass to CSS
             "sass-loader",
         ],
     };
